da3461c50a58c537d6c1c74a240d9a9e0f45400e
praveenaj2026 <praveenaj2026@users.noreply.github.com>
2026-04-23 21:23:53 +0530

    Initial commit

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-GameCore Monitor - Gaming & System Performance Intelligence
-Version: 0.1.0
-"""
-
-__version__ = "0.1.0"
-__author__ = "Praveen"
-__description__ = "Advanced PC Health & Gaming Performance Monitor"
--- src/alerts/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Alert System Package
-"""
-
-from .alert_system import Alert, AlertLevel, AlertRule, AlertManager, create_default_rules
-
-__all__ = ['Alert', 'AlertLevel', 'AlertRule', 'AlertManager', 'create_default_rules']
